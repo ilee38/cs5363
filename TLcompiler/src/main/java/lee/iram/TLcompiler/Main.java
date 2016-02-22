d2fa0c907464e567d7cfb42de8a6b825621fb1f6
Iram Lee <iram.lee@gmail.com>
2016-02-21 19:49:36 -0600

    Added Main and Scanner classes

---
+++ TLcompiler/src/main/java/lee/iram/TLcompiler/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package lee.iram.TLcompiler;
+
+import java.io.InputStream;
+
+/**
+ * @author iramlee
+ * Main class of the program, entry point to the compiler
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		try{
+			String srcCodeFileName = args[0];
+		} catch(Exception e){}
+
+	}
+
+}
---
+++ TLcompiler/src/main/java/lee/iram/TLcompiler/Scanner.java
@@ -0,0 +1,9 @@
+package lee.iram.TLcompiler;
+
+/**
+ * @author iramlee
+ *	Scanner class. Performs the lexical analysis on the source code file
+ */
+public class Scanner {
+
+}

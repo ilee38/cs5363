c9c9a8afa7c7f6478dc1a0fc6de97636823b6810
i_lee <iram.lee@gmail.com>
2016-02-25 13:23:00 -0600

    Created constructor and file reader on Scanner class

--- TLcompiler/src/main/java/lee/iram/TLcompiler/Scanner.java
+++ TLcompiler/src/main/java/lee/iram/TLcompiler/Scanner.java
@@ -1,9 +1,33 @@
 package lee.iram.TLcompiler;
 
+import java.io.*;
+
 /**
  * @author iramlee
  *	Scanner class. Performs the lexical analysis on the source code file
  */
 public class Scanner {
-
+	String fileName;
+	
+	//Scanner constructor
+	public Scanner(String file){
+		fileName = file;
+	}
+	
+	//Generate text file containing the list of tokens
+	public void getTokenList(){
+		BufferedReader reader = openFile(fileName);
+	}
+	
+	//Return the file reader object
+	public BufferedReader openFile(String fileName){
+		BufferedReader rd = null;
+		try{
+			rd = new BufferedReader(new FileReader(fileName));
+		}catch (IOException ex){
+			System.out.println("Can't open that file");
+		}
+		return rd;
+	}
 }
+

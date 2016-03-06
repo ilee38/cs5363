2ccc1f5482e1fa5fedf4e519c1f75a7f73273b1d
Iram Lee <iram.lee@gmail.com>
2016-03-06 17:32:24 -0600

    finished implementation of the Scanner class

--- TLcompiler/src/main/java/lee/iram/TLcompiler/InitScannerTables.java
+++ TLcompiler/src/main/java/lee/iram/TLcompiler/InitScannerTables.java
@@ -15,54 +15,82 @@ import java.util.*;
  */
 public class InitScannerTables {
 	
-	private Map<String, String> initStateTransTable = new HashMap<String, String>();
-	private Map<String, String> tokenTypeTable = new HashMap<String, String>();
-	private Set<String> keywords = new HashSet<String>();
+	private ArrayList<HashMap<String, Integer>> scanTable = new ArrayList<HashMap<String, Integer>>();
+	private Map<Integer, String> tokenTypeTable = new HashMap<Integer, String>();
+	private Map<String, String> keywords = new HashMap<String, String>();
+	
+	private HashMap<String, Integer> state_0 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_1 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_2 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_3 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_4 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_5 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_6 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_7 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_8 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_9 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_10 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_11 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_12 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_13 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_14 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_15 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_16 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_17 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_18 = new HashMap<String, Integer>();
+	private HashMap<String, Integer> state_19 = new HashMap<String, Integer>();
 	
 //Constructor
-	public InitScannerTables(){}
+	public InitScannerTables(){
+	}
 	
-//Creates the transition table for state 0 (initial state)
-		public Map<String, String> getTransTable(){
-			initStateTransTable.put("(", "1");
-			initStateTransTable.put(")", "2");
-			initStateTransTable.put(":", "3");
-			initStateTransTable.put(";", "5");
-			initStateTransTable.put("*", "6");
-			initStateTransTable.put("+", "7");
-			initStateTransTable.put("-", "8");
-			initStateTransTable.put("=", "9");
-			initStateTransTable.put("!", "10");
-			initStateTransTable.put("<", "12");
-			initStateTransTable.put(">", "14");
-			initStateTransTable.put("lc", "16");	//lc = any lower case character
-			initStateTransTable.put("UC", "17");	//UC = any upper case character
-			initStateTransTable.put("num", "18");	//num = any number from 1-9
-			initStateTransTable.put("0", "19");
-			return initStateTransTable;
-		}
-		
+//Creates the scanner table containing the state transitions
+	public ArrayList<HashMap<String, Integer>> getScanTable(){
+		fillStateTables();
+		scanTable.add(state_0);
+		scanTable.add(state_1);
+		scanTable.add(state_2);
+		scanTable.add(state_3);
+		scanTable.add(state_4);
+		scanTable.add(state_5);
+		scanTable.add(state_6);
+		scanTable.add(state_7);
+		scanTable.add(state_8);
+		scanTable.add(state_9);
+		scanTable.add(state_10);
+		scanTable.add(state_11);
+		scanTable.add(state_12);
+		scanTable.add(state_13);
+		scanTable.add(state_14);
+		scanTable.add(state_15);
+		scanTable.add(state_16);
+		scanTable.add(state_17);
+		scanTable.add(state_18);
+		scanTable.add(state_19);
+		return scanTable;
+	}
+	
 //Creates a table to match the type of token that was recognized
 //Associates the ending state of the token to the token type (see Scanner transition table
 //Excel file)
-		public Map<String, String> getTokenTypeTable(){
-			tokenTypeTable.put("1", "LP");
-			tokenTypeTable.put("2", "RP");
-			tokenTypeTable.put("4", "ASGN");
-			tokenTypeTable.put("5", "SC");
-			tokenTypeTable.put("6", "MULTIPLICATIVE(*)");
-			tokenTypeTable.put("7", "ADDITIVE(+)");
-			tokenTypeTable.put("8", "ADDITIVE(-)");
-			tokenTypeTable.put("9", "COMPARE(=)");
-			tokenTypeTable.put("11", "COMPARE(!=)");
-			tokenTypeTable.put("12", "COMPARE(<)");
-			tokenTypeTable.put("13", "COMPARE(<=)");
-			tokenTypeTable.put("14", "COMPARE(>)");
-			tokenTypeTable.put("15", "COMPARE(>=)");
-			tokenTypeTable.put("16", "KEYWORD");	//should return the keyword itself (in upper case)
-			tokenTypeTable.put("17", "ident");	//should return ident+(xyz..)
-			tokenTypeTable.put("18", "num");	//should return num+(123..)
-			tokenTypeTable.put("19", "num");	//state for number zero "0" Should return num+(0)
+		public Map<Integer, String> getTokenTypeTable(){
+			tokenTypeTable.put(1, "LP");
+			tokenTypeTable.put(2, "RP");
+			tokenTypeTable.put(4, "ASGN");
+			tokenTypeTable.put(5, "SC");
+			tokenTypeTable.put(6, "MULTIPLICATIVE");
+			tokenTypeTable.put(7, "ADDITIVE");		// +
+			tokenTypeTable.put(8, "ADDITIVE");		// -
+			tokenTypeTable.put(9, "COMPARE");		// =
+			tokenTypeTable.put(11, "COMPARE");		// !=
+			tokenTypeTable.put(12, "COMPARE");		// <
+			tokenTypeTable.put(13, "COMPARE");		// <=
+			tokenTypeTable.put(14, "COMPARE");		// >
+			tokenTypeTable.put(15, "COMPARE");		// >=
+			tokenTypeTable.put(16, "KEYWORD");		//should return the keyword itself (in lower case)
+			tokenTypeTable.put(17, "ident");		//should return ident+(XYZ..)
+			tokenTypeTable.put(18, "num");			//should return num+(123..)
+			tokenTypeTable.put(19, "num");			//state for number zero "0" Should return num+(0)
 			return tokenTypeTable;
 		}
 		
@@ -71,25 +99,85 @@ public class InitScannerTables {
 //boollit = "false", "true"
 //MULTIPLICATIVE = "div", "mod"
 //"writeint" and "readint"
-		public Set<String> getKeywordTable(){
-			keywords.add("false");
-			keywords.add("true");
-			keywords.add("div");
-			keywords.add("mod");
-			keywords.add("if");
-			keywords.add("then");
-			keywords.add("else");
-			keywords.add("begin");
-			keywords.add("end");
-			keywords.add("while");
-			keywords.add("do");
-			keywords.add("program");
-			keywords.add("var");
-			keywords.add("as");
-			keywords.add("int");
-			keywords.add("bool");
-			keywords.add("writeint");
-			keywords.add("readint");
+		public Map<String, String> getKeywordTable(){
+			keywords.put("false", "boollit(false)");
+			keywords.put("true", "boollit(true)");
+			keywords.put("div", "MULTIPLICATIVE(div)");
+			keywords.put("mod", "MULTIPLICATIVE(mod)");
+			keywords.put("if", "IF");
+			keywords.put("then", "THEN");
+			keywords.put("else", "ELSE");
+			keywords.put("begin", "BEGIN");
+			keywords.put("end", "END");
+			keywords.put("while", "WHILE");
+			keywords.put("do", "DO");
+			keywords.put("program", "PROGRAM");
+			keywords.put("var", "VAR");
+			keywords.put("as", "AS");
+			keywords.put("int", "INT");
+			keywords.put("bool", "BOOL");
+			keywords.put("writeint", "WRITEINT");
+			keywords.put("readint", "READINT");
 			return keywords;
 		}
+		
+//Fills out the HashMaps for each state, indicating the next transition on a given
+//input character
+		private void fillStateTables(){
+			state_0.put("(", 1);
+			state_0.put(")", 2);
+			state_0.put(":", 3);
+			state_0.put(";", 5);
+			state_0.put("*", 6);
+			state_0.put("+", 7);
+			state_0.put("-", 8);
+			state_0.put("=", 9);
+			state_0.put("!", 10);
+			state_0.put("<", 12);
+			state_0.put(">", 14);
+			state_0.put("lc", 16);
+			state_0.put("UC", 17);
+			state_0.put("digit", 18);
+			state_0.put("0", 19);
+			state_0.put(" ", 0);
+			
+			state_1.put(" ", 1);
+			state_2.put(" ", 2);
+			state_3.put("=", 4);
+			state_4.put(" ", 4);
+			state_5.put(" ", 5);
+			state_6.put(" ", 6);
+			state_7.put(" ", 7);
+			state_8.put(" ", 8);
+			state_9.put(" ", 9);
+			state_10.put("=", 11);
+			state_11.put(" ", 11);
+			
+			state_12.put("=", 13);
+			state_12.put(" ", 12);
+			
+			state_13.put(" ", 13);
+			
+			state_14.put("=", 15);
+			state_14.put(" ", 14);
+			
+			state_15.put(" ", 15);
+			
+			state_16.put("lc", 16);
+			state_16.put(" ", 16);
+			
+			state_17.put("UC", 17);
+			state_17.put("digit", 17);
+			state_17.put("0", 17);
+			state_17.put(" ", 17);
+			
+			state_18.put("digit", 18);
+			state_18.put("0", 18);
+			state_18.put(" ", 18);
+			
+			state_19.put(" ", 19);
+		}
 }
+
+
+	
--- TLcompiler/src/main/java/lee/iram/TLcompiler/Scanner.java
+++ TLcompiler/src/main/java/lee/iram/TLcompiler/Scanner.java
@@ -9,18 +9,29 @@ import java.util.*;
  *	Generates a text file containing the list of tokens in the source code
  */
 public class Scanner {
-	private String fileName;
-	private Map<String, String> transitionTable;
-	private Map<String, String> tokenType;
-	private Set<String> keywordList;
-	private ArrayList<String> bufferBlock = new ArrayList<String>();
 	
-//Scanner constructor
-	public Scanner(){
-		
+	private final String OUTPUT_FILE_NAME = "ScannerOutput.txt"; 
+	
+	private String fileName;
+	private boolean syntaxError = false;
+	private int lineCount = 0;
+	private ArrayList<HashMap<String, Integer>> scannerTable = new ArrayList<HashMap<String, Integer>>();
+	private Map<Integer, String> tokenType;
+	private Map<String, String> keywordList;
+	private ArrayList<String> bufferBlock = new ArrayList<String>();
+	private PrintWriter writer = createFile(OUTPUT_FILE_NAME);
+	
+/*	
+ * Scanner constructor
+ * 
+ */
+	public Scanner(){	
 	}
 	
-//Starting method of the Scanner class: Generates a text file containing the list of tokens
+/*
+ * Starting method of the Scanner class: Generates a text file containing the list of tokens
+ * 
+ */
 	public void writeTokenList(String file){
 		this.fileName = file;
 		String inputLine;
@@ -30,115 +41,109 @@ public class Scanner {
 			while(true){
 				inputLine = reader.readLine();
 				if (inputLine == null)break;
+				if(inputLine.contains("%")){					//check if line of code contains comments
+					int index = inputLine.indexOf("%");
+					inputLine = inputLine.substring(0, index);	//remove the comment part of the line
+				}
 				bufferBlock = fillBuffer(inputLine);
-				System.out.println(bufferBlock);
+				lineCount++;
+				if(syntaxError)return;
 				findTokens(bufferBlock);
 			}
 		} catch (IOException e) {
 			System.out.println("File error");
 			e.printStackTrace();
-		} 
-		
+		}
+		writer.close();
 	}
-	
-//Analyzes a line of source code to find the available tokens. Outputs each token
-//into a text file, or generates a Scanner Error message if there is a lexical
-//error
+
+/*
+ * Analyzes a line of source code to find the available tokens. Outputs each token
+ * into a text file, or generates a Scanner Error message if there is a lexical
+ * error
+ * 
+ */
 	private void findTokens(ArrayList<String> bufferBlock){
-		String curChar, curState, nextChar, nextState, token;
-		curState = "0"; nextState = "0"; token = "";		//Initialize current state to initial state
+		String curChar, token, readChar; String lexeme = "";
+		Integer curState;
+	//*	Stack<String> rememberedChars = new Stack<String>();
+	//*	Stack<Integer> rememberedState = new Stack<Integer>();
+		curState = 0; token = "";						//Initialize current state to initial state
 		for(int i = 0; i < bufferBlock.size(); i++){
-			curChar = bufferBlock.get(i);
-			if(curChar.equals("#"))break;
-			System.out.println("curChar: " + curChar);
-			if(transitionTable.containsKey(curChar)){		//Check if valid input for state 0
-				nextState = transitionTable.get(curChar);
-				System.out.println("next state: "+nextState);
-				if(tokenType.containsKey(nextState)){		//Check if next state is an accepting state
-					nextChar = bufferBlock.get(i+1);		//do lookahead 1 character
-					if((nextChar.equals(" ")) || (nextChar.equals("#"))){
-						token = curChar;
-						i++;	//skip blank space
-						System.out.println("token: "+token);
-					//	saveToken(token);
-					}else{
-						System.out.println("SYNTAX ERROR");
-					}
+			if(syntaxError)return;
+			readChar = bufferBlock.get(i);
+			curChar = updateChar(readChar);																		//check if read char is lower case, uper case or digit
+			if(scannerTable.get(curState).containsKey(curChar) || curChar.equals("#")){							//Check if character is valid or sentinel for end of buffer block
+				if(tokenType.containsKey(curState) && (readChar.equals(" ") || i == bufferBlock.size()-1)){		//Check if accepting state and if lookahead char is space or sentinel
+					//recognize Token
+			 		token = tokenType.get(curState);
+					i--;								//unread current char (i.e. decrease index of bufferBlock)
+					lookupLexeme(token, lexeme);		
+					curState = 0;						//restart curState
+					lexeme = "";						//clear lexeme for next token
 				}else{
-					token += curChar;
-					curState = nextState;
-					System.out.println("partial token: "+token);
-				//	findNextState(token, curState, i);	//Find transition for non-initial state
+					//take transition
+				//*	if(tokenType.containsKey(curState)){
+				//*		rememberedState.push(curState);
+				//*		rememberedChars.push("");
+				//*	}
+				//*	rememberedChars.push(readChar);
+					curState = scannerTable.get(curState).get(curChar);
+					lexeme += readChar;
 				}
 			}else{
-				System.out.println("SYNTAX ERROR");
+				//check Error
+			//*	if(!rememberedState.empty()){
+			//*		token = tokenType.get(rememberedState.pop());
+			//*		String delChar = rememberedChars.pop();		//unread remembered chars
+					//remove remembered chars from lexeme??
+			//*		curState = 0;
+			//*	}
+				System.out.println("SYNTAX ERROR: found on line " + lineCount);
+				syntaxError = true;
 			}
 		}
 	}
-	
-//Determines the transitions for states other than the initial state.
-	private void findNextState(String token, String curState, int blockIndex){
-		String curChar, nextState;
-		curChar = bufferBlock.get(blockIndex + 1);
-		switch (curState){
-			case "3":
-				if(curChar == "="){
-					nextState = "4";
-					token += curChar;
-					doLookahead(nextState, token);
-				}else{
-					System.out.println("SYNTAX ERROR");
-				}
-				break;
-			case "10":
-				if(curChar == "="){
-					nextState = "11";
-					token += curChar;
-					doLookahead(nextState, token);
-				}else{
-					System.out.println("SYNTAX ERROR");
-				}
-				break;
-			case "12":
-				if(curChar == "="){
-					nextState = "13";
-					token += curChar;
-					doLookahead(nextState, token);
-				}else if (curChar == " "){
-					nextState = "12";
-					doLookahead(nextState, token);
-				}else{
-					System.out.println("SYNTAX ERROR");
-				}
-				break;
-			case "14":
-				if(curChar == "="){
-					nextState = "15";
-					token += curChar;
-					doLookahead(nextState, token);
-				}else if (curChar == " "){
-					nextState = "14";
-					doLookahead(nextState, token);
-				}else{
-					System.out.println("SYNTAX ERROR");
-				}
-				break;
-			case "16":
-				int charNum = curChar.charAt(0);	//Convert the character to its integer representation
-				if((charNum >= 'a') && (charNum <= 'z')){
-					nextState = "16";
-					token += curChar;
-					
-				}
+
+/*	
+ * Checks if the input character is a lower case letter, upper case letter or a digit from 1 to 9
+ * 
+ */
+	private String updateChar(String chr){
+		if(chr.charAt(0) >= 'a' && chr.charAt(0) <= 'z'){
+			return "lc"; 
+		}else if(chr.charAt(0) >= 'A' && chr.charAt(0) <= 'Z'){
+			return "UC";
+		}else if(chr.charAt(0) >= '1' && chr.charAt(0) <= '9'){
+			return "digit";
+		}else if(chr.equals("\t") || chr.equals("\n")){		//treat tabs or new lines as single white space
+			return " ";
+		}else{
+			return chr;
+		}
+	}
+
+/*
+ * Checks if the lexeme is a keyword, identifier or digit. Then outputs the token on appropriate format based
+ * on the BNF grammar. The token is saved on the output file.
+ * 
+ */
+	private void lookupLexeme(String token, String lexeme){
+		lexeme = lexeme.trim();			//remove white spaces from lexeme
+		if(token.equals("KEYWORD")){
+			writer.println(keywordList.get(lexeme));
+		}else if (token.equals("ident") || token.equals("num") || token.equals("MULTIPLICATIVE")
+				|| token.equals("ADDITIVE") || token.equals("COMPARE")){
+			writer.println(token + "("+lexeme+")");
+		}else{
+			writer.println(token);
 		}
 	}
 	
-//Performs lookahead
-	private void doLookahead(String nextState, String token){
-		
-	}
-
-//Returns an ArrayList with each character in the input line of code
+/*
+ * Returns an ArrayList where each element represents each character in the input line of code
+ * 
+ */
 	private ArrayList<String> fillBuffer(String inputLine){
 		ArrayList<String> bufBlock = new ArrayList<String>();
 		for(int i = 0; i < inputLine.length(); i++){
@@ -149,27 +154,43 @@ public class Scanner {
 		return bufBlock;
 	}
 	
-//Returns a file reader object
+/*
+ * Returns a file reader object
+ * 
+ */
 	private BufferedReader openFile(String fileName){
 		BufferedReader rd = null;
 		try{
 			rd = new BufferedReader(new FileReader(fileName));
-			System.out.println("File opened successfully");
 		}catch (IOException ex){
 			System.out.println("Can't open that file");
 		}
 		return rd;
 	}
-	
-//Creates the 3 tables for the scanner
+
+/*	
+ * Returns a file writer object
+ * 
+ */
+	private PrintWriter createFile(String fileName){
+		PrintWriter wr = null;
+		try{
+			wr = new PrintWriter(new FileWriter("ScannerOutput.txt"));
+		} catch(IOException ex){
+			System.out.println("Error writing file");
+		}
+		return wr;
+	}
+
+/*	
+ * Creates the 3 tables for the scanner
+ * 
+ */
 	private void initializeTables(){
-		InitScannerTables scannerTables = new InitScannerTables();	
-		transitionTable = scannerTables.getTransTable();
+		InitScannerTables scannerTables = new InitScannerTables();
+		scannerTable = scannerTables.getScanTable();
 		tokenType = scannerTables.getTokenTypeTable();
 		keywordList = scannerTables.getKeywordTable();
-		System.out.println(transitionTable.toString());
-		System.out.println(tokenType.toString());
-		System.out.println(keywordList.toString());
 	}
 }
 
